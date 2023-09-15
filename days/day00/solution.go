152b1ad31269f3d95dfbcf0a5488b28cf53cc193
Mikkel <mikkel@housekeepr.dk>
2023-09-15 20:58:55 +0200

    add template

---
+++ days/day00/solution.go
@@ -0,0 +1,21 @@
+package day00
+
+import (
+	"fmt"
+
+	"github.com/mikkj17/aoc2018/utils"
+)
+
+func partOne(inp string) int {
+	return 0
+}
+
+func partTwo(inp string) int {
+	return 0
+}
+
+func Solve() {
+	input := utils.ReadInput(0)
+	fmt.Println(partOne(input))
+	fmt.Println(partTwo(input))
+}
